9bc27dbd213054c987c57bb1a55d0c5e6b0ec37a
bernesepeck <mirjamthomet@gmail.com>
2024-01-27 16:07:09 +0100

    add basic directus api request

--- frontend/index.ts
+++ frontend/index.ts
@@ -3,5 +3,8 @@ import { City } from "./city/city";
 function render() : void {
     const city = new City();
     document.body.appendChild(city);
+    const banner = document.createElement('div');
+    banner.className="banner"
+    city.appendChild(banner);
 }
 render();
