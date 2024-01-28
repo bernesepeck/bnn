837d79976313a3d38b6f42fb693c6a6407109e79
bernesepeck <mirjamthomet@gmail.com>
2024-01-28 19:57:02 +0100

    add content container

--- frontend/city/city.ts
+++ frontend/city/city.ts
@@ -1,51 +1,47 @@
-import { LitElement, html, css } from "lit";
+import { html } from "lit";
 import { customElement, state } from "lit/decorators.js";
 import "../components/banner/banner";
 import "../components/language/languageSelector";
 import { CityModel } from "./city.models";
 import { CityService } from "./city.service";
 import { DefaultComponent } from "../components/default.component";
+import "../components/content-container/content-container";
 
 @customElement("bnn-city")
 export class City extends DefaultComponent {
-    private cityService: CityService;
+  private cityService: CityService;
 
-    static get componentStyles() {
-        return css`
-            h1 {
-              color: blue;
-            }
-          `; 
-    }
+  constructor() {
+    super();
+    const languageCode = sessionStorage.getItem("selectedLanguage");
+    this.cityService = new CityService(languageCode);
+  }
 
-    constructor() {
-        super();
-        const languageCode = sessionStorage.getItem('selectedLanguage')
-        this.cityService = new CityService(languageCode);
-    }
+  @state()
+  protected city: CityModel | undefined;
 
-    @state()
-    protected city: CityModel | undefined;
+  connectedCallback() {
+    super.connectedCallback();
 
-    connectedCallback() {
-        super.connectedCallback();
-        
-        this.cityService.getCity(2).then(cityData => {
-            this.city = cityData;
-            console.log(cityData)
-        }).catch(error => {
-            console.error("Failed to fetch city data", error);
-            // Handle the error appropriately
-        });
-    }
+    this.cityService
+      .getCity(2)
+      .then((cityData) => {
+        this.city = cityData;
+        console.log(cityData);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch city data", error);
+        // Handle the error appropriately
+      });
+  }
 
-    public render() {
-        return html`
-            <bnn-banner .cityname="${this.city?.name}"></bnn-banner>
-            <div class="header">
-                <h2>${this.city?.page_title}</h2>
-                <p>${this.city?.description}</p>
-            </div>
-        `;
-    }
+  public render() {
+    return html`
+      <bnn-banner .cityname="${this.city?.name}"></bnn-banner>
+      <bnn-content-container>
+        <h2>${this.city?.page_title}</h2>
+        <p>${this.city?.description}</p>
+      </bnn-content-container>
+    `;
+  }
 }
--- frontend/components/language/languageSelector.ts
+++ frontend/components/language/languageSelector.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit';
+import { html, css } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { DefaultComponent } from '../default.component';
 
