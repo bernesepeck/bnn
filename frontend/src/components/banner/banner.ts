9b9361d22a4509c9030faf19e36c4c9c0d9ad614
bernesepeck <mirjamthomet@gmail.com>
2024-02-10 08:23:04 +0100

    feat: add home data

--- frontend/src/components/banner/banner.ts
+++ frontend/src/components/banner/banner.ts
@@ -1,47 +1,72 @@
-import {  html, css } from "lit";
+import { html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { DefaultComponent } from "../default.component";
 import "../menu/nav-menu";
+import { classMap } from "lit/directives/class-map";
 
 @customElement("bnn-banner")
 export class Banner extends DefaultComponent {
   @property({ type: String })
   cityname?: string;
 
+  @property({ type: String })
+  description?: string;
+
   static get componentStyles() {
     return css`
-    .header-wrapper {
-        background-image: linear-gradient(rgba(0, 0, 0, 0.25), rgba(165, 147, 160, 0.73)), url(/bnn-banner.webp);
+      .header-wrapper {
+        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(30, 33, 72, 0.6)), url(/bnn-banner.webp);
+          url(/bnn-banner.webp);
         height: var(--header-size);
         padding: 0px var(--content-padding);
         padding-top: 16px;
         display: flex;
         justify-content: center;
         background-size: cover;
-    }
-    .max-content {
-      width: 100%;
-      max-width: 1000px;
-      display: flex;
-      flex-direction: column;
-    }
-    bnn-nav-menu {
-      margin-left: auto;
-    }
-    h1 {
+      }
+
+      .has-description {
+        --header-size: 650px;
+        --font-size-xl: 40px;
+        & h1 {
+          background-color: var(--color-primary)
+        }
+        & p {
+          color: white;
+          font-size: var(--font-size-l);
+        }
+      }
+      .max-content {
+        width: 100%;
+        max-width: 1000px;
+        display: flex;
+        flex-direction: column;
+      }
+      bnn-nav-menu {
+        margin-left: auto;
+      }
+      h1 {
         margin-top: 0;
         color: white;
-        padding-top: 2em;
-    }
-  `
+        margin-top: 2em;
+      }
+    `;
   }
 
   public render() {
     return html`
-      <div class="header-wrapper">
+      <div
+        class="${classMap({
+          "header-wrapper": true,
+          "has-description": !!this.description?.length,
+        })}"
+      >
         <div class="max-content">
           <bnn-nav-menu></bnn-nav-menu>
           <h1>${this.cityname}</h1>
+          ${this.description?.length
+            ? html` <p>${this.description}</p> `
+            : html``}
         </div>
       </div>
     `;
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,25 +1,66 @@
-import { customElement } from "lit/decorators";
+import { customElement, state } from "lit/decorators";
 import { DefaultComponent } from "./components/default.component";
-import { html } from "lit";
-import './components/banner/banner';
-import './components/footer/footer';
+import { css, html } from "lit";
+import "./components/banner/banner";
+import "./components/footer/footer";
+import "./components/content-container/content-container";
 import { ConfigService } from "./config-service";
+import { HomeModel, HomeService } from "./home.service";
+import { classMap } from 'lit/directives/class-map.js';
 
-@customElement('bnn-home')
+@customElement("bnn-home")
 export class Home extends DefaultComponent {
-    constructor() {
-        super();
-        const configService = ConfigService.getInstance();
-        configService.loadConfig().then(() => {
-          console.log("Configuration loaded successfully.");
-        }).catch(error => {
-          console.error("Failed to load configuration:", error);
-        });
+  @state()
+  home: HomeModel | undefined;
+
+  static get componentStyles() {
+    return css`
+      .grid-container {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: var(--gutter-m);
       }
-    public render() {
-        return html`
-            <bnn-banner></bnn-banner>
-            <bnn-footer></bnn-footer>
-        `
-    }
+      .full-width {
+        grid-column: span 2;
+      }
+
+      .half-width {
+        grid-column: span 1;
+      }
+    `;
+  }
+
+  constructor() {
+    super();
+    const configService = ConfigService.getInstance();
+    configService
+      .loadConfig()
+      .then(async () => {
+        console.log("Configuration loaded successfully.");
+        const homeService = new HomeService();
+        this.home = await homeService.getHome();
+      })
+      .catch((error) => {
+        console.error("Failed to load configuration:", error);
+      });
+  }
+  public render() {
+    return html`
+      <bnn-banner
+        .cityname="${this.home?.titel}"
+        .description="${this.home?.description}"
+      ></bnn-banner>
+      <bnn-content-container>
+        <div class="grid-container">
+          ${this.home?.contentbox.map((content) => {
+            return html`<div class="${classMap({'content-box': true, 'full-width': content.width === 1, 'half-width': content.width === 0.5})}">
+              <h2>${content.title}</h2>
+              <p .innerHTML="${content.description}"></p>
+            </div>`;
+          })}
+        </div>
+      </bnn-content-container>
+      <bnn-footer></bnn-footer>
+    `;
+  }
 }
