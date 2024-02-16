0a0f386d425559c04dda27d234bbd4b57c2feb92
bernesepeck <mirjamthomet@gmail.com>
2024-02-16 11:10:54 +0100

    fix translation and config loading

--- frontend/src/config-service.ts
+++ frontend/src/config-service.ts
@@ -4,46 +4,45 @@ export type AppConfig = {
 
 declare var process: {
   env: {
-      NODE_ENV: string;
-      PARCEL_API_URL?: string;
-  }
+    NODE_ENV: string;
+    PARCEL_API_URL?: string;
+  };
 };
 
 export class ConfigService {
   private static instance: ConfigService;
-  public configLoaded: boolean = false;
   private config!: AppConfig;
 
   public static getInstance(): ConfigService {
-      if (!ConfigService.instance) {
-          ConfigService.instance = new ConfigService();
-      }
-      return ConfigService.instance;
+    if (!ConfigService.instance) {
+      ConfigService.instance = new ConfigService();
+    }
+    return ConfigService.instance;
   }
 
   public async fetchConfig() {
-      this.configLoaded = true;
-      if (process.env.NODE_ENV !== 'development') {
-          // In development, use environment variables directly.
-          this.config = {
-              apiUrl: process.env.PARCEL_API_URL || '',
-          };
+    if (!this.config) {
+      if (process.env.NODE_ENV !== "development") {
+        // In development, use environment variables directly.
+        this.config = {
+          apiUrl: process.env.PARCEL_API_URL || "",
+        };
       } else {
-          // In production or other environments, fetch the config.json.
-          try {
-            const response = await fetch('/config.json');
-            const responseJson = await response.json();
-            this.config = {
-                apiUrl: responseJson.apiUrl || '',
-            };
-          } catch (error) {
-            console.error("Failed to load config:", error);
-            this.configLoaded = false;
-          } 
+        // In production or other environments, fetch the config.json.
+        try {
+          const response = await fetch("/config.json");
+          const responseJson = await response.json();
+          this.config = {
+            apiUrl: responseJson.apiUrl || "",
+          };
+        } catch (error) {
+          console.error("Failed to load config:", error);
+        }
       }
+    }
   }
 
   public getConfig(): AppConfig {
-      return this.config;
+    return this.config;
   }
 }
--- frontend/src/services/translation.service.ts
+++ frontend/src/services/translation.service.ts
@@ -2,66 +2,68 @@ import { createDirectus, readItems, rest } from "@directus/sdk";
 import { AppConfig } from "../config";
 
 export interface TranslationModel {
-    key: string, 
-    text: string
+  key: string;
+  text: string;
 }
 
 export class TranslationService {
-    private static instance: TranslationService;
-    public translationsLoaded: boolean = false;
-    private client;
-    private langFilter;
-    private translations: TranslationModel[] = [];
+  private static instance: TranslationService;
+  private client;
+  private langFilter;
+  private translations: TranslationModel[] | undefined;
 
-    constructor(config: AppConfig) {
-        this.client = createDirectus(config.apiUrl).with(rest());
-        console.log('constructor', config)
-        const languageCode = sessionStorage.getItem('selectedLanguage') || 'de';
-        this.langFilter = {
-            translations: {
-                _filter: {
-                    languages_code: {_eq: languageCode}
-                }
-            }
-        }
+  constructor(config: AppConfig) {
+    this.client = createDirectus(config.apiUrl).with(rest());
+    const languageCode = sessionStorage.getItem("selectedLanguage") || "de";
+    this.langFilter = {
+      translations: {
+        _filter: {
+          languages_code: { _eq: languageCode },
+        },
+      },
+    };
+  }
+
+  public static getInstance(config: AppConfig): TranslationService {
+    if (!TranslationService.instance) {
+      TranslationService.instance = new TranslationService(config);
     }
+    return TranslationService.instance;
+  }
 
-    public static getInstance(config: AppConfig): TranslationService {
-        if (!TranslationService.instance) {
-            TranslationService.instance = new TranslationService(config);
-        }
-        return TranslationService.instance;
-    }
-
-    async fetchTranslations() {
-        this.translationsLoaded = true;
-        const response = await this.client.request(readItems('GeneralTranslations', {
-            fields: [
-                'key',
-                'translations.text'
-            ],
+  async fetchTranslations() {
+    if (!this.translations) {
+      const response = await this.client
+        .request(
+          readItems("GeneralTranslations", {
+            fields: ["key", "translations.text"],
             deep: {
-                ...this.langFilter,
-            }
-        })).catch(() => this.translationsLoaded = false)
-        
-        this.translations = response.map(t => ({key: t.key, text: t.translations[0].text}))
-    }
-    
-    getTranslations(): TranslationModel[] {
-        return this.translations;
-    }
+              ...this.langFilter,
+            },
+          })
+        )
 
-    getTranslation(key: string): string {
-        if(!this.translations.length) {
-            return '';
-        }
-        const translation = this.translations.find(t => t.key === key);
-        if(translation) {
-            return translation.text;
-        } else {
-            console.error('translation doesnt exist')
-            return '';
-        }
+      this.translations = response.map((t) => ({
+        key: t.key,
+        text: t.translations[0].text,
+      }));
     }
+  }
+
+  getTranslations(): TranslationModel[] {
+    return this.translations ?? [];
+  }
+
+  getTranslation(key: string): string {
+    if (!this.translations?.length) {
+      return "";
+    }
+    const translation = this.translations.find((t) => t.key === key);
+    if (translation) {
+      return translation.text;
+    } else {
+      console.error("translation doesnt exist");
+      return "";
+    }
+  }
 }
