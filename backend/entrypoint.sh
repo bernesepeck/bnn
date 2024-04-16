cc636c57fe1d847fa109d7062db2a4ac85947159
John Gannon <john.gannon@liip.ch>
2024-04-16 21:20:21 +0200

    Modify dokcerfile and entrypoint to allow directus to run its own migrations before applying our schema migration

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# Check if we need to apply a Directus schema snapshot
+# Start Directus normally in the background
+node cli.js bootstrap &
+directus_pid=$!
+
+# Wait for Directus to finish bootstrapping
+wait $directus_pid
+
+# Now apply Directus schema snapshot if needed
 if [ "$APPLY_SNAPSHOT" = "true" ]; then
   echo "Downloading and applying schema snapshot..."
   wget -O snapshot-latest.yaml https://storage.googleapis.com/bnn-directus-snapshots/snapshot-latest.yaml
   npx directus schema apply --yes ./snapshot-latest.yaml
 fi
 
-# Start Directus normally
-echo "Executing command: $@"
-exec "$@"
+# Continue to run Directus in the foreground
+exec pm2-runtime start ecosystem.config.cjs
